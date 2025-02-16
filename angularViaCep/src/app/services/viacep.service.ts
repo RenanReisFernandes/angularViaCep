80680667cf904c5f441153f0124b17b2a2618990
Renan Reis <renanmacfernandes@hotmail.com>
2025-02-16 15:25:21 -0300

    adicionando a rota do formulario no app-routing

--- angularViaCep/src/app/services/viacep.service.ts
+++ angularViaCep/src/app/services/viacep.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { ViaCepResult } from '../models/ViaCepResult';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +12,14 @@ export class ViacepService {
   apiUrl: string = environment.viacepUrl;
 
   constructor(private http: HttpClient) { }
+
+  getEnderecoByCep(cep: string){
+    return this.http.get<ViaCepResult>
+    (this.apiUrl + cep + "/json/")
+    .pipe(
+      map((response)=>{
+        return response;
+      })
+    )
+  }
 }
